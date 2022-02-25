0c9e28bc7a7a4a44080fe0b960d0b26b1ec0edac
Yale Wei <longanw@gmail.com>
2022-02-25 16:53:10 +0800

    重新设计改版

---
+++ service/md5.command
@@ -0,0 +1,14 @@
+#!/bin/sh
+mv CJJ\ T110-2017_建筑与小区管道直饮水系统技术规程.pdf 7984c84bf85403266378a392cb77b6a4.pdf
+mv CJJ\ T154-2020建筑给水金属管道工程技术标准.pdf 5ad145063221e09b0e9a05059c3d0757.pdf
+mv CJT\ 151-2016\ 薄壁不锈钢管.pdf 6bca2a6521b748950e5ecb228724a235.pdf
+mv CJT\ 152-2016\ 薄壁不锈钢卡压式和沟槽式管件.pdf 66ab8f3ddd4968a7e42617ddaf2b4174.pdf
+mv CJT\ 466-2014双卡压燃气管件.pdf 29c7e6d9a33fa9de017fd534de26ba07.pdf
+mv GB\ 50015-2019建筑给水排水设计规范.pdf 5780f972873e72a7d50ddbb4a51d1ad9.pdf
+mv GBT\ 12771-2019流体输送用不锈钢焊接钢管.pdf 9826df1ba0e37da4a467a29411cfb770.pdf
+mv GBT\ 19228.1-2011\ 不锈钢卡压式管件组件\ 第1部分\ 卡压式管件.pdf ac901042db6edd667168ff8c157a2e9f.pdf
+mv GBT\ 19228.2-2011不锈钢卡压式管件组件\ 第2部分\ 连接用薄壁不锈钢管.pdf 49bbeafb35c76da2b3d86162e5378e2a.pdf
+mv GBT\ 19228.3-2012不锈钢卡压式管件组件\ 第3部分\ O型橡胶密封圈.pdf 9dae34b91352802660705caff814229f.pdf
+mv GBT\ 29038-2012\ 薄壁不锈钢管道技术规范.pdf 0494e2cda38f8ac38c671b15bc3e7c41.pdf
+mv GBT\ 33926-2017\ 不锈钢环压式管件.pdf 181620f318404829f16a79e05a34bffe.pdf
+mv GBT_50378-2019_绿色建筑评价标准_最新_完整版.pdf bb5f571ce50456b895bf11fe2f266baa.pdf
---
+++ static/js/layout.js
@@ -0,0 +1,172 @@
+﻿$(function () {
+    //返回顶部
+    $('.to-top').on('click', function () {
+        $('html,body').stop().animate({
+            'scrollTop': 0
+        }, 200); //数字动画开始于离页面底部的距离
+    });
+    //数字滚动效果
+    function numscroll(e) {
+        var i = e.parents("div").offset().top,
+            t = e.attr("bolExec");
+        $(document).scrollTop() > i - 3 * $(window).height() / 4 && !t && (! function () {
+            e.text(e.text().replace(".", ""));
+            var i = 0,
+                t = e.text() - 0,
+                n = (t + "").length,
+                s = setInterval(function () {
+                    i++;
+                    for (var o = parseInt(t / 100 * i), a = "", l = 0; l < n - (o + "").length; l++) a += 0;
+                    e.text(a + o);
+                    if (i >= 100) {
+                        e.text(e.attr("rel"));
+                        clearInterval(s)
+                    }
+                }, 50); //动画时长
+        }(), e.attr("bolExec", !0))
+    }
+    /*统一固定高度*/
+    //页面滚动
+    function winScroll() {
+        var t_top = $(window).scrollTop();
+        $(".num").each(function (i) {
+            var wrapTop = $(this).offset().top;
+            if (t_top > wrapTop - 800) {
+                numscroll($(this));
+            }
+        });
+    }
+    $(window).on('scroll', function () {
+        winScroll();
+    });
+
+});
+
+//表单验证并发送至指定邮箱
+//当用户名失去焦点时
+function blur_fullname() {
+    var user_value = document.getElementsByName("fullname")[0].value;
+    if (user_value.length === 0) {
+        document.getElementById("user_res").innerHTML = "<font color='#f00'>您没有输入姓名</font>";
+        return false;
+    } else if ($.trim(user_value).length < 2) {
+        document.getElementById("user_res").innerHTML = "<font color='#f00'>姓名不少于两个字</font>";
+        return false;
+    } else {
+        document.getElementById("user_res").innerHTML = "<font color='#00f'>OK</font>";
+        return true;
+    }
+}
+
+//手机号码失去焦点时
+function blur_phone() {
+    var user_value = document.getElementsByName("phone")[0].value;
+    var pattern = /^1[3456789]\d{9}$/; //验证手机号正则表达式
+    if (user_value.length === 0) {
+        document.getElementById("phone_res").innerHTML = "<font color='#f00'>您没有输入手机号码</font>";
+        return false;
+    } else if (!pattern.test(user_value)) {
+        document.getElementById("phone_res").innerHTML = "<font color='#f00'>手机号码有误，请核对</font>";
+        return false;
+    } else {
+        document.getElementById("phone_res").innerHTML = "<font color='#00f'>OK</font>";
+        return true;
+    }
+}
+
+//电子邮箱失去焦点时
+function blur_email() {
+    var user_value = document.getElementsByName("email")[0].value;
+    var pattern = /^\S+@\S+\.\S+$/; //验证电子邮箱正则表达式
+    if (user_value.length === 0) {
+        document.getElementById("email_res").innerHTML = "<font color='#f00'>您没有输入电子邮箱</font>";
+        return false;
+    } else if (!pattern.test(user_value)) {
+        document.getElementById("email_res").innerHTML = "<font color='#f00'>电子邮箱有误，请核对</font>";
+        return false;
+    } else {
+        document.getElementById("email_res").innerHTML = "<font color='#00f'>OK</font>";
+        return true;
+    }
+}
+
+//当留言失去焦点时
+function blur_message() {
+    var user_value = document.getElementsByName("message")[0].value;
+
+        if (user_value.length === 0) {
+        document.getElementById("message_res").innerHTML = "<font color='#f00'>您没有输入内容</font>";
+        return false;
+    } else if ($.trim(user_value).length < 2) {
+        document.getElementById("message_res").innerHTML = "<font color='#f00'>留言不少于两个字</font>";
+        return false;
+    } else {
+        document.getElementById("message_res").innerHTML = "<font color='#00f'>OK</font>";
+        return true;
+    }
+}
+
+//提交提交表单验证
+function checkForm() {
+    var user_flag = blur_fullname();
+    var phone_flag = blur_phone();
+    var email_flag = blur_email();
+    var message_flag = blur_message();
+    if (user_flag && phone_flag && email_flag && message_flag) {
+        return true;
+    } else {
+        alert("输入有误，请按要求修改再提交。");
+        return false;
+    }
+}
+
+function sendEmail(from, subject, body) {
+
+    Email.send({
+        SecureToken: "c49f358a-44b9-40cf-b59a-ed4f0593f624",
+        To: 'longanw@163.com',
+        From: from,
+        Subject: subject,
+        Body: body
+    }).then(
+        message => {
+            if (message == 'OK') {
+                document.getElementById("contact-form").reset();
+                alert('留言成功，感谢您的支持。');
+            } else {
+                console.error(message);
+                alert('抱歉，服务出了点差错，欢迎继续尝试，谢谢！')
+            }
+        });
+}
+
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const subject = `${data.get("fullname")} 先生/女士( ${data.get("phone")} )的留言`;
+    const body = `留言如下：<br> ${data.get("message")} <br> 电话：${data.get("phone")} <br> 邮箱: ${data.get("email")} `;
+    var form_status = checkForm();
+    if (form_status) {
+        sendEmail("longanw@gmail.com", subject, body);
+    } else {
+
+    }
+});
+
+//清除提示
+form.addEventListener("reset", (e) => {
+    $("#user_res").empty();
+    $("#phone_res").empty();
+    $("#email_res").empty();
+    $("#message_res").empty();
+});
+
+$('.container .loadMore').loadMoreResults();
+
+var url;
+
+function openFile(url) {
+    var fileDir = "..%2F..%2F..%2Fservice%2F"
+    window.open("static/pdfjs/web/viewer.html?file=" + fileDir + url);
+};
