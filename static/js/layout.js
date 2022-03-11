cfe74df63b6e167de52ca88bb592481a438f67d1
Yale Wei <longanw@gmail.com>
2022-03-11 09:24:52 +0800

    再优化资源加载,压缩文件

--- static/js/layout.js
+++ static/js/layout.js
@@ -31,7 +31,6 @@
 		banner slider Carousel
  	------------------------------- */
     $("#banner").owlCarousel({
-        lazyLoad: true,
         loop: true,
         dots: true,
         nav: true,
@@ -61,7 +60,6 @@
 		Services Carousel
  	------------------------------- */
     $("#services-slider").owlCarousel({
-        lazyLoad: true,
         loop: true,
         dots: true,
         autoHeight: true,
@@ -91,7 +89,6 @@
 		Cases Carousel
  	------------------------------- */
     $("#cases-slider").owlCarousel({
-        lazyLoad: true,
         autoplay: true,
         autoplayHoverPause: true,
         autoplayTimeout: 3000,
@@ -131,7 +128,6 @@
 		News Carousel
  	------------------------------- */
     $("#news").owlCarousel({
-        lazyLoad: true,
         autoplay: true,
         autoplayHoverPause: true,
         autoplayTimeout: 8000,
@@ -522,13 +518,21 @@ $(function () {
                 }
 
                 for (var i = offset; i < (offset + size); i++) {
-                    result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-card-border pmd-z-depth"><div class="cert pmd-card-media pmd-card-media-overlay"><img src="' +
-                        data[i].pic + '" alt="' +
-                        data[i].alt + '" title="' +
-                        data[i].title + '" class="img-responsive" />' +
-                        '</div></div></div>';
-                }
+                    if (i < size) {
+                        result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-card-border pmd-z-depth"><div class="cert pmd-card-media pmd-card-media-overlay"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="' +
+                            data[i].pic + '" alt="' +
+                            data[i].alt + '" title="' +
+                            data[i].title + '" class="lazyload img-responsive" />' +
+                            '</div></div></div>';
+                    } else {
+                        result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-card-border pmd-z-depth"><div class="cert pmd-card-media pmd-card-media-overlay"><img src="' +
+                            data[i].pic + '" alt="' +
+                            data[i].alt + '" title="' +
+                            data[i].title + '" class="img-responsive" />' +
+                            '</div></div></div>';
+                    }
 
+                }
                 $('.lists-cert').append(result);
 
                 /*隐藏more*/
@@ -582,11 +586,19 @@ $(function () {
                 }
 
                 for (var i = offset; i < (offset + size); i++) {
-                    result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-z-depth"><div class="pmd-card-media"><img src="' +
-                        data[i].pic + '" alt="' +
-                        data[i].alt + '" title="' +
-                        data[i].title + '" class="img-responsive" />' +
-                        '</div></div></div>';
+                    if (i < size) {
+                        result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-z-depth"><div class="pmd-card-media"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="' +
+                            data[i].pic + '" alt="' +
+                            data[i].alt + '" title="' +
+                            data[i].title + '" class="lazyload img-responsive" />' +
+                            '</div></div></div>';
+                    } else {
+                        result += '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><div class="pmd-card pmd-z-depth"><div class="pmd-card-media"><img src="' +
+                            data[i].pic + '" alt="' +
+                            data[i].alt + '" title="' +
+                            data[i].title + '" class="img-responsive" />' +
+                            '</div></div></div>';
+                    }
                 }
 
                 $('.lists-partner').append(result);
@@ -608,63 +620,63 @@ $(function () {
 
 });
 
-$(function () {
-
-    /*初始化*/
-    var counter = 0; /*计数器*/
-    var pageStart = 0; /*offset*/
-    var pageSize = 6; /*size*/
-
-    getStdData(pageStart, pageSize);
-
-    /*监听加载更多*/
-    $(document).on('click', '#load-more-standards', function () {
-        counter++;
-        pageStart = counter * pageSize;
-
-        getStdData(pageStart, pageSize);
-    });
-
-    function getStdData(offset, size) {
-        $.ajax({
-            type: 'GET',
-            url: 'static/json/standards.json',
-            dataType: 'json',
-            success: function (reponse) {
-
-                var data = reponse.lists;
-                var sum = reponse.lists.length;
-
-                var result = '';
-
-                if (sum - offset < size) {
-                    size = sum - offset;
-                }
-
-                for (var i = offset; i < (offset + size); i++) {
-                    result += '<div class="lb_thumb" style="margin: 20px"> <div class = "lb_book-cover" style = "background-image: url("' +
-                        data[i].pic + '" );" onclick="javascript:openFile("' +
-                        data[i].src + '");"><span class="lb_book-title">' +
-                        data[i].title + '</span></div> </div>';
-                }
-
-                $('.lists-standards').append(result);
-
-                /*隐藏more*/
-                if ((offset + size) >= sum) {
-                    $("#load-more-standards").hide();
-                } else {
-                    $("#load-more-standards").show();
-                }
-            },
-            error: function (xhr, type) {
-                setTimeout(function () {
-                    $.hulla.send("&#9889服务器未返回数据！", "warning");
-                }, 1000);
-            }
-        });
-    }
-});
+//$(function () {
+//
+//    /*初始化*/
+//    var counter = 0; /*计数器*/
+//    var pageStart = 0; /*offset*/
+//    var pageSize = 6; /*size*/
+//
+//    getStdData(pageStart, pageSize);
+//
+//    /*监听加载更多*/
+//    $(document).on('click', '#load-more-standards', function () {
+//        counter++;
+//        pageStart = counter * pageSize;
+//
+//        getStdData(pageStart, pageSize);
+//    });
+//
+//    function getStdData(offset, size) {
+//        $.ajax({
+//            type: 'GET',
+//            url: 'static/json/standards.json',
+//            dataType: 'json',
+//            success: function (reponse) {
+//
+//                var data = reponse.lists;
+//                var sum = reponse.lists.length;
+//
+//                var result = '';
+//
+//                if (sum - offset < size) {
+//                    size = sum - offset;
+//                }
+//
+//                for (var i = offset; i < (offset + size); i++) {
+//                    result += '<div class="lb_thumb" style="margin: 20px"> <div class = "lb_book-cover" style = "background-image: url("' +
+//                        data[i].pic + '" );" onclick="javascript:openFile("' +
+//                        data[i].src + '");"><span class="lb_book-title">' +
+//                        data[i].title + '</span></div> </div>';
+//                }
+//
+//                $('.lists-standards').append(result);
+//
+//                /*隐藏more*/
+//                if ((offset + size) >= sum) {
+//                    $("#load-more-standards").hide();
+//                } else {
+//                    $("#load-more-standards").show();
+//                }
+//            },
+//            error: function (xhr, type) {
+//                setTimeout(function () {
+//                    $.hulla.send("&#9889服务器未返回数据！", "warning");
+//                }, 1000);
+//            }
+//        });
+//    }
+//});
 
 (function ($) {
     $.fn.btnLoadmore = function (options) {
@@ -717,8 +729,8 @@ $(function () {
 
 $(document).ready(function () {
     $('.contents').btnLoadmore({
-        showItem: 8,
-        whenClickBtn: 6,
+        showItem: 6,
+        whenClickBtn: 4,
         textBtn: '--- 加载更多 ---'
     });
 });
